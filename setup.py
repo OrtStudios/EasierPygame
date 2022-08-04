55adeb24c4e957ef133585e16d3ee44d34f20370
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-08-04 09:58:02 +0300

    log of staff to do

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
+# change this every time you make a new release
 VERSION = '0.1.1'
 DESCRIPTION = 'the python library that makes PYGAME easier to use'
 
